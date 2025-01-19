51a91c03a7a405d67c09e86b01e9b954bbe05a1c
MateusSNeubarth <mateusneubarth@hotmail.com>
2025-01-19 12:18:26 -0300

    Finished main, kicked off pass_gen, but not much did

--- src/main.py
+++ src/main.py
@@ -1,3 +1,36 @@
+import os
 from simple_calculator import calculator
 
-calculator()
+print("Welcome to Python Basics, some basic examples of Python Algorithms!")
+
+while True:
+    os.system("cls")
+    print("1 - Simple Calculator")
+    print("2 - Password Generator")
+    print("3 - Unity Conversor")
+    print("4 - Guessing Game")
+    print("5 - Timer")
+    print("X - Exit")
+    input = input("Please, select the algorithm you'd like to see")
+
+    match input:
+        case "1":
+            os.system("cls")
+            calculator()
+        case "2":
+            os.system("cls")
+            print("TO DO - Password Generator")
+        case "3":
+            os.system("cls")
+            print("TO DO - Unity Conversor")
+        case "4":
+            os.system("cls")
+            print("TO DO - Guessing Game")
+        case "5":
+            os.system("cls")
+            print("TO DO - Timer")
+        case "X":
+            break
+
+
+print("Thanks for utlizing my app!")
---
+++ src/pass_gen.py
@@ -0,0 +1,14 @@
+"""
+PASSWORD GENERATOR
+
+This app is able to create a random and secure password for your accounts using your own pre-requisites
+"""
+
+SPECIAL_CHARS = "!@#$%¨&*()-_+="
+NUMS = "1234567890"
+CAP_LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+LETTERS = "abcdefghijklmonpqrstuvwxyz"
+
+"""
+TODO 
+"""
